4a8363eb2ad8dc128d9057e5aebe2f6d08e5a4bd
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2023-10-11 18:42:42 +0530

    Create To get list of all columns of a table.php

---
+++ All Queries/To get list of all columns of a table.php
@@ -0,0 +1,51 @@
+<?php
+$list = $this->acts_model->get_datatables();
+
+
+// Acts Model Code to get the list
+ 
+        function get_datatables()
+        {
+           $this->_get_datatables_query();
+            if(isset($_POST['length']) && $_POST['length'] != -1)
+            $this->db->limit($_POST['length'], $_POST['start']);
+            $query = $this->db->get();
+            return $query->result();
+        }
+        // Get Database 
+         public function _get_datatables_query()
+        {     
+            $this->db->from($this->table);
+            $this->joinQuery();
+            $this->db->select('acts.title,acts.act_number,acts.status,acts.dt,acts.id,ac.title as categoty_title,asc.title as sub_category_title');
+        
+            $i = 0;     
+            foreach ($this->column_search as $item) // loop column 
+            {
+                if(isset($_POST['search']['value']) && $_POST['search']['value']) // if datatable send POST for search
+                {
+                    if($i===0) // first loop
+                    {
+                        $this->db->like($item, $_POST['search']['value']);
+                    }
+                    else
+                    {
+                        $this->db->or_like($item, $_POST['search']['value']);
+                    }
+                }
+                $i++;
+            }
+             
+            if(isset($_POST['order'])) // here order processing
+            {
+                $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+            } 
+            else if(isset($this->order))
+            {
+                $order = $this->order;
+                $this->db->order_by(key($order), $order[key($order)]);
+            }
+            $this->action_condition();//call action condition function
+            
+        }
+?>
