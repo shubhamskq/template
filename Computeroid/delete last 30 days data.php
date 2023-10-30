b4e9d8d9a398d5ef502d4bc315a4bb3af854c391
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2023-10-30 11:04:53 +0530

    Create delete last 30 days data.php

---
+++ Computeroid/delete last 30 days data.php
@@ -0,0 +1,6 @@
+<?php
+      $date_threshold = date('Y-m-d H:i:s', strtotime('-60 days'));
+        $sql = "DELETE FROM analytics WHERE DateAt < '$date_threshold'";
+        $this->analitics_model->rawQuery($sql);
+
+?>
