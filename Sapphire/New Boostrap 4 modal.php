eb19770528f1fd0c7f0f2b59ab46669f8a3035d1
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2024-01-09 15:20:13 +0530

    Create New Boostrap 4 modal.php

---
+++ Sapphire/New Boostrap 4 modal.php
@@ -0,0 +1,112 @@
+ <div class="modal" id="alert_modal" tabindex="-1" role="dialog">
+      <div class="modal-dialog mod_dog_wd" role="document">
+          <div class="modal-content">
+          <button type="button" class="close close_css_modle" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                  </button>
+              <div class="modal-body     text-center">
+                  <h3 class="alert-heading_ ">We have noticed a spurt in spurious Outbound Calls</h3>
+                  <p class="text-dark"><b>Voice Mails and E-mails going out to our customers by unauthorized
+                          callers/companies posing themselves as Sapphire Softech Solutions LLC or claiming to be
+                          associated with us.</b></p>
+                  <h6>Please beware if you hear anything like listed below.</h6>
+
+                  <div class="imges__ mt-4">
+                      <img src="<?php echo $base_url; ?>assets/img/alert3.jpg" alt="sapphiresoftech" />
+                      <img src="<?php echo $base_url; ?>assets/img/alert4.jpg" alt="sapphiresoftech" />
+                      <button type="button" class="alert-btn bg-red mt-4" name="" id="">Scammer Calling</button>
+
+                  </div>
+                  <div class="audio_con text-center mt-2">
+                      <h3>Scam Call Example..</h3>
+                      <audio style="width: 90%" controls="controls" id="player">
+                          <source src="media/scam_call_alert.mp3" type="audio/mp3">
+                      </audio>
+                      <p>
+                          <a href="http://www.nomorobo.com/lookup/877-874-3174" target="_blank"
+                              style="color: #4aa9dc;font-size: 20px;">
+                              <b>Click Here To Nomorobo Protection For Scammer Call</b>
+                          </a>
+                      </p>
+                  </div>
+                  <div class="text-center alert-content-padding">
+                      <h5 class="mt-2"><b><u>DON'T SHARE YOUR SCREEN OR BANKING DETAILS AT ANY COST</u></b></h5>
+                  </div>
+                  <div class="alert-content-padding clon_edit_chg">
+
+                      <P class="mt-2"><b>SAPPHIRE SOFTECH IS ONLY A CALL AWAY</b>, IF IN DOUBT CALL US ON <b> OR </b>
+                      </P>
+
+                      <ul class="list-styled modal_ul">
+                          <li>
+                              They Call you Posing to be from <b class="text-red">MICROSOFT REFUND DEPARTMENT</b> and
+                              tell you that company you paid to is <b class="text-dark">CLOSING DOWN</b> and won't share
+                              name of company.
+                          </li>
+                          <li>
+                              They would claim that <b class="text-red">MICROSOFT SERVER HAS CRASHED</b> and they are
+                              refunding money, it's a <b class="text-dark">TRAP. DON'T GIVE ACCESS TO YOUR SYSTEM OR
+                                  BANKING OR SHARE ANYTHING, EVEN LAST 2 OR 4 DIGITS</b>
+                          </li>
+                          <li>
+                              They would tell you that they have refunded <b class="text-dark">ACCESS Money, $3000 or
+                                  $4000</b> & pressurize you pay them back.
+                          </li>
+
+                          <li>
+                              You'll be asked to <span clas="text-red"> Pay via </span><b class="text-red"><u>ITUNE
+                                      CARDS, WALMART PRE PAID CARDS OR BANK WIRE OR MONEY GRAM.</u></b>
+                          </li>
+
+                          <li>
+                              Your giving access to system could compromise your personal data and security. So, <b
+                                  class="text-red">DON'T SHARE YOUR COMPUTER ACCESS WITH ANYONE.</b>
+                          </li>
+                          <li>
+                              <b class="text-dark">CALL SAPPHIRE ON FOR INSTANT HELP +1-888-357-5222 OR +1-888-573-4222
+                              </b>
+                          </li>
+
+                      </ul>
+
+
+
+
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <!-- Modal -->
+  
+
+
+  <!-- Modal2 -->
+  <div class="modal fade" id="alert_modal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle"
+      aria-hidden="true">
+      <div class="modal-dialog" role="document">
+          <div class="modal-content">
+              <div class="modal-header" style="padding-top: 0px;">
+                  <button type="button" class="close closeBtnSpecial" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true" style="font-size: 30px;">&times;</span>
+                  </button>
+
+
+
+              </div>
+
+
+
+              <div class="modal-body">
+                  <!-- Content-->
+                  <div class="text-center alert-content-padding" style="padding: 5em 0;">
+                      <h3 class="alert-heading ">We have no association with <br />Sapphire Solutions INC</h3>
+                  </div>
+                  <!-- //Content-->
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary closeBtnSpecial " data-dismiss="modal">Close</button>
+              </div>
+          </div>
+      </div>
+  </div>
