4562d8090af662ff3851f7e3f91f5f380b1bc133
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2024-01-23 18:03:53 +0530

    Create unset value in array.php

---
+++ Insaaf99/unset value in array.php
@@ -0,0 +1,9 @@
+<?php
+
+if(in_array($code,$selected_languages)){    
+  $t =  array_search($code, $selected_languages);
+  unset($selected_languages[$t]);
+                                                }
+
+
+?>
