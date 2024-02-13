8d33e65993686606cfe5302a2cbbe083fbbe6c98
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2024-02-13 14:53:15 +0530

    Update hidden div.php

--- Insaaf99/hidden div.php
+++ Insaaf99/hidden div.php
@@ -2,7 +2,7 @@
 
 // footer
 
-    <div id="myDiv" style="text-align: center; width: 110%; height: 150%; display: none; z-index: 999; top: 100 %; left: 50%; transform: translate(-50%, -50%); position: absolute; background: rgba(0,0,0,.5);">
+    <div id="myDiv" style="text-align: center; width: 110%; height: 150%; display: none; z-index: 999; top: 100px; left: 50%; transform: translate(-50%, -50%); position: absolute; background: rgba(0,0,0,.5);">
              <img style="margin-top: 750px; width: 50px;" src="<?php echo base_url('assets/images/loader.gif')?>">
              <p style="font-size: 18px; color: white; margin-right: 10px;">Please Wait</p>
     </div>
