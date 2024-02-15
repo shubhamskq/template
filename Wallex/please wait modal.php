6e35b6bbccfd7e6d1bd6025eb5f29df6737b4e26
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2024-02-15 17:38:19 +0530

    Create please wait modal.php

---
+++ Wallex/please wait modal.php
@@ -0,0 +1,6 @@
+<!-- Please Wait Modal -->
+    <div class="modal fade" data-backdrop="static" id="exampleModal1" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                   <img style="width: 50px;margin-left: 48%; margin-top: 12%;" src="<?php echo base_url('assets/images/loader.gif')?>">
+             <p style="font-size: 18px; color: white; margin-right: 10px;margin-left: 47%;">Please Wait</p>
+                </div>
